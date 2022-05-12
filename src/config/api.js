2d21d1296d5b51cc7d615e3f90f291670aa5f301
Kevin <kevinity310@gmail.com>
2022-05-12 21:45:32 +0700

    change url

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // buat base URL
 export const API = axios.create({
-  baseURL:  "http://localhost:5000/api/v1/",
+  baseURL: process.env.SERVER_URL || "https://e-dumbmerch.herokuapp.com/api/v1" ||  "http://localhost:5000/api/v1/",
 });
 // process.env.SERVER_URL || "https://e-dumbmerch.herokuapp.com/api/v1" ||
 
